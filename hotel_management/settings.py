5c279e00cc13aa23b6e03a0c8c367b629e8bfdad
Kad Denuwara <137709290+KadDenuwara@users.noreply.github.com>
2024-04-20 05:25:09 +0530

    Update settings.py

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -170,7 +170,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-DEBUG = True
+#DEBUG = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

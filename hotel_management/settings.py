9c5ffc25d1225bfe579f9e3244f01bc0c840bdb3
Kad Denuwara <137709290+KadDenuwara@users.noreply.github.com>
2024-04-20 11:58:59 +0530

    Update settings.py

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -165,14 +165,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+'''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#DEBUG = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+'''
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
